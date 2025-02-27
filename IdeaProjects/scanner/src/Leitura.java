3ad66ed64fa7a89c4add9e3a4a0a20034ce06e67
viktor <162162844+vikxzzzz@users.noreply.github.com>
2025-02-27 11:38:08 -0300

    Add files via upload

---
+++ IdeaProjects/scanner/src/Leitura.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        int idade;
+        float peso;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Qual a sua idade?");
+        idade = teclado.nextInt();
+        System.out.println("Qual a seu peso?");
+        peso = teclado.nextFloat();
+        System.out.println("Idade: " + idade + "\n" + "Peso: " + peso);
+    }
+}
