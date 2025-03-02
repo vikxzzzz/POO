7a8c614dc1be64d18336d0c4b08a3f5aa57500e9
viktor <162162844+vikxzzzz@users.noreply.github.com>
2025-03-02 12:55:47 -0300

    exercicios-java-1-4

---
+++ exercicios-slide-1-4/src/Ex3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args){
+        float salario;
+        float aumento;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe seu salário:");
+        salario = teclado.nextFloat();
+        System.out.println("Informe a % do aumento:");
+        aumento = teclado.nextFloat();
+
+         float novoSalario = salario + (salario * aumento / 100);
+        System.out.println("Novo salário:" + novoSalario);
+    }
+}
