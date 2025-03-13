8cf63d98d52fc1b739c98980e8c86b41e2770cc2
viktor <162162844+vikxzzzz@users.noreply.github.com>
2025-03-13 11:29:25 -0300

    Add files via upload

--- circulo/src/Circulo.java
+++ circulo/src/Circulo.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class Circulo {
-    public static void main(String[] args){
-        final float PI = 3.14F;
+    public static void main(String[] args) {
         float raio;
 
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite o valor do raio:");
         raio = teclado.nextFloat();
 
-        float perimetro = 2 * PI * raio;
-        float area = PI *(raio * raio);
+        double perimetro = 2 * Math.PI * raio;
+        double area = Math.PI * (raio * raio);
 
-        System.out.println("Perímetro:" + perimetro);
-        System.out.println("Área:" + area);
+        System.out.printf("Perímetro: %.2f%n", perimetro);
+        System.out.printf("Área: %.2f%n", area);
 
     }
 }
